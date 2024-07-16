1fe21af5517f793b17fb446d93da53218b93fbc2
fakhryhizballah <falehry88@gmail.com>
2024-07-16 15:22:01 +0700

    history warna

--- public/js/history.js
+++ public/js/history.js
@@ -9,42 +9,12 @@ $("#description").text(description);
 
 let dataTable = $('#tabelClient').DataTable({
     ajax: "/api/netwatch/v1/member/" + getHistory.id,
-    // "dataSrc": function (json) {
-    //     console.log('Received data:', json);
-    //     // Periksa struktur data yang diterima dan ekstrak 'history'
-    //     if (json.message === "success" && json.data && Array.isArray(json.data.history)) {
-    //         return json.data.history;
-    //     } else {
-    //         console.error('Unexpected data format:', json);
-    //         throw new Error('Unexpected data format');
-    //     }
-    // },
     dataSrc: {
         data: 'data',
     },
 
     // data: [
-    // {
-    //     "id": 4092,
-    //     "idMembers": 5,
-    //     "lastUpdate": "2024-07-15T23:01:25.000Z",
-    //     "status": true,
-    //     "duration": "a few seconds (Offline Time)",
-    //     "uptime": 3269,
-    //     "createdAt": "2024-07-15T23:01:25.000Z",
-    //     "updatedAt": "2024-07-15T23:01:25.000Z"
-    // },
-    // {
-    //     "id": 4091,
-    //     "idMembers": 5,
-    //     "lastUpdate": "2024-07-15T23:01:22.000Z",
-    //     "status": false,
-    //     "duration": "2 days (Online Time)",
-    //     "uptime": 165206680,
-    //     "createdAt": "2024-07-15T23:01:22.000Z",
-    //     "updatedAt": "2024-07-15T23:01:22.000Z"
-    // }
-    // ],
+
     columns: [
         { data: function (data, type, row, meta) { return meta.row + 1 } },
         {
@@ -56,5 +26,13 @@ let dataTable = $('#tabelClient').DataTable({
         },
         { data: function (data) { return (data.status) ? "Online" : "Offline" } },
         { data: 'duration' },
-    ]
+    ],
+    createdRow: function (row, data, dataIndex) {
+        console.log(row);
+        if (data.status) {
+            $(row).addClass('table-success');
+        } else {
+            $(row).addClass('table-danger');
+        }
+    },
 });
